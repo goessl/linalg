d5bfd30e808c0a67376a1e28777d12e113050049
Sebastian Gössl <goessl@student.tugraz.at>
2024-10-09 17:19:21 +0200

    Row Echelon, Rank Decomposition, Pseudoinverse

--- linalg.py
+++ linalg.py
@@ -1,6 +1,7 @@
 import numpy as np
 from functools import reduce
 from operator import add, mul
+from itertools import pairwise
 from fractions import Fraction
 from tqdm.auto import tqdm
 
@@ -26,6 +27,23 @@ def randf(shape=None, precision=1000):
     n = randint(-precision, +precision+1, shape)
     d = randint(1, +precision+1, shape)
     return Fraction(n, d) if shape is None else np.vectorize(Fraction)(n, d)
+
+def randfr(M, N, r, precision=1000):
+    """Return a random MxN `Fraction` matrix of rank `r`."""
+    #https://math.stackexchange.com/a/757864/1170417
+    U, V = randf((M, M), precision), randf((N, N), precision)
+    P = np.zeros((M, N), dtype=np.int64)
+    P[np.diag_indices(r)] = 1
+    return U @ P @ V
+
+
+if __name__ == '__main__':
+    for _ in range(100):
+        M, N = randint(2, 10, size=2)
+        r = randint(0, min(M, N)+1)
+        A = randfr(M, N, r)
+        
+        assert np.linalg.matrix_rank(A.astype(np.float64)) == r
 
 
 
@@ -635,3 +653,125 @@ if __name__ == '__main__':
                 and np.array_equal(P@L@U@Q, A)
         assert M<=N and np.all(np.diag(L)==1) \
                 or M>N and np.all(np.diag(U)==1)
+
+
+
+
+
+# - - - Row Echelon, Rank Decomposition, Pseudoinverse - - -
+def is_ref(A, reduced=True):
+    """Return if `A` is of (reduced) row echelon form."""
+    assert_matrix(A)
+    pivots = [next((i for i, a in enumerate(r) if a), A.shape[1]) for r in A]
+    #check all pivots ascend to the right
+    if not all(pi<pj or pj==A.shape[1] for pi, pj in pairwise(pivots)):
+        return False
+    
+    if reduced:
+        #check if pivots one and zeros above
+        for i, p in enumerate(pivots):
+            if p<A.shape[1]:
+                if A[i, p]!=1 or np.any(A[:i, p]):
+                    return False
+    return True
+
+def ref_gauss(A, reduced=True):
+    """Transform `A` into (reduced) row echelon form.
+    
+    By Gaussian elimination with pivoting.
+    Transforms `A` in place.
+    For a MxN-matrix of rank r there will be
+    - $MNr-Nr(r+1)/2$ subtractions (`-`),
+    - $MNr-Nr(r+1)/2$ multiplications (`*`),
+    - $Mr-r(r+1)/2$ divisions (`/`),
+    - so $Mr(2N+1)-Nr(r+1)-r(r+1)/2$ operations in total.
+    """
+    #https://en.wikipedia.org/wiki/Gaussian_elimination#Pseudocode
+    i, j = 0, 0
+    pivots = []
+    while i<A.shape[0] and j<A.shape[1]:
+        #find pivot
+        if not A[(p := np.argmax(abs(A[i:, j])) + i), j]:
+            j += 1
+        else:
+            #pivot
+            swap_rows(A, i, p)
+            if reduced:
+                #normalize pivot
+                A[i, :] /= A[i, j]
+                #zeros above and below
+                A[:i, :] -= A[i, :] * A[:i, j][:, np.newaxis]
+                A[i+1:, :] -= A[i, :] * A[i+1:, j][:, np.newaxis]
+            else:
+                #zeros below
+                A[i+1:, :] -= A[i, :] * (A[i+1:, j] / A[i, j])[:, np.newaxis]
+            pivots += [j]
+            i += 1
+            j += 1
+    return pivots
+
+
+if __name__ == '__main__':
+    for _ in range(100):
+        N = randint(1, 10)
+        A = randf((N, N))
+        
+        ref_gauss(A, False)
+        assert is_ref(A, False)
+        
+        A = randf((N, N))
+        ref_gauss(A, True)
+        assert is_ref(A, True)
+
+
+
+def rank_decomposition(A):
+    """Return a rank decomposition `B, C` of `A` such that `A=BC`."""
+    #https://en.wikipedia.org/wiki/Rank_factorization#Rank_factorization_from_reduced_row_echelon_forms
+    pivots = ref_gauss(C:=A.copy())
+    #delete non pivot columns from A
+    B = np.delete(A, [i for i in range(A.shape[1]) if i not in pivots], 1)
+    #delete zero rows from C
+    for i in reversed(range(C.shape[0])):
+        if np.all(np.logical_not(C[i, :])):
+            C = np.delete(C, i, 0)
+    return B, C
+
+def pinv(A):
+    """Return the Moore–Penrose inverse of `A`."""
+    #https://en.wikipedia.org/wiki/Moore%E2%80%93Penrose_inverse#Rank_decomposition
+    B, C = rank_decomposition(A)
+    return C.T @ inv_gauss(C@C.T) @ inv_gauss(B.T@B) @ B.T
+
+def lstsq(X, y):
+    """Return the linear least squares solution `b` for `y=Xb`."""
+    return pinv(X.T@X) @ X.T @ y
+
+
+if __name__ == '__main__':
+    for _ in range(100):
+        M, N  = randint(1, 10, size=2)
+        r = randint(0, min(M, N)+1)
+        A = randfr(M, N, r)
+        B, C = rank_decomposition(A)
+        
+        assert B.shape==(M, r) and C.shape==(r, N) and np.array_equal(A, B@C)
+    
+    for _ in range(100):
+        M, N = randint(1, 10, size=2)
+        r = randint(0, min(M, N)+1)
+        A = randfr(M, N, r)
+        
+        Ainv = pinv(A)
+        #https://en.wikipedia.org/wiki/Moore%E2%80%93Penrose_inverse#Definition
+        assert np.array_equal(A@Ainv@A, A) \
+                and np.array_equal(Ainv@A@Ainv, Ainv) \
+                and np.array_equal((A@Ainv).conj().T, A@Ainv) \
+                and np.array_equal((Ainv@A).conj().T, Ainv@A)
+        
+        X, y = randf((M, N)), randf(M)
+        
+        prediction = lstsq(X, y)
+        actual = np.linalg.lstsq(
+                X.astype(np.float64), y.astype(np.float64), rcond=None)[0]
+        assert np.allclose(prediction.astype(np.float64), actual)
